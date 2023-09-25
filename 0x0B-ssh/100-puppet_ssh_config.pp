8a4856bffed02ab3836a6f809ab0ded8a7cb0051
prosperemebo <emeboprosper@gmail.com>
2023-09-25 05:01:02 +0100

    Updated IPs

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,8 @@
 # Set up server configuration with puppet
 file_line{'Set alias name':
   path => '/etc/ssh/ssh_config',
-  line => 'Host 107.23.218.234
-    HostName 107.23.218.234
+  line => 'Host 54.172.59.160
+    HostName 54.172.59.160
     User ubuntu
     IdentityFile ~/.ssh/school
     PasswordAuthentication no',
